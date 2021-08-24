25b167cf9b4fa58f77ee06a0deb6cb509a30b0cf
Anne Porter <a.porter9212@gmail.com>
2021-08-23 22:16:38 -0400

    message

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("postgres://postgres:PASSWORD@localhost:5432/chaos-chronicle");
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT ==='production'
+});
 
 module.exports = sequelize;
